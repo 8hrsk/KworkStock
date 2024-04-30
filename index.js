2fb6bd66835e576b13dbb82f605763202eeb1e2c
= <=>
2024-05-01 01:25:09 +0300

    selectors and parser

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const links = require('./links.json'); // kwork links
+const selectors = require('./selectors.json'); // kwork stock selectors
 require('dotenv').config();
 
 const Bot = require('./bot/Bot');
 const Parser = require('./parser/Parser');
 
-const parser = new Parser(links);
+const parser = new Parser(links, selectors);
 const bot = new Bot(process.env.TELEGRAM_BOT_TOKEN, process.env.TELEGRAM_CHANNEL_NAME);
+
+setInterval(() => {
+    
+}, 180000);
--- parser/Parser.js
+++ parser/Parser.js
@@ -2,9 +2,10 @@ const axios = require('axios');
 const { JSDOM } = require('jsdom');
 
 class Parser {
-    constructor(links) {
+    constructor(links, selectors) {
+        this.selectors = selectors;
         this.dataArray = [];
-        
+
         links.forEach((link) => {
             this.get(link, (data) => {
                 this.parseDom(data)
@@ -21,11 +22,36 @@ class Parser {
 
     parseDom(htmlData) { // parse HTML into jsdom
         const dom = new JSDOM(htmlData)
+        this.getDataFromDOM(dom)
     }
 
-    constructObject() {} // set a js Object from recieved data
+    constructObject(data) { // set a js Object from recieved data
 
+    }
 
+    getDataFromDOM(dom) {
+        const latestStock = dom.window.document.querySelector(this.selectors.stockSelector)
+
+        this.StockChildren = this.getChildren(latestStock)
+    }
+
+    getChildren(element) {
+        return Array.from(element.children)
+    }
+
+    getDataFromAllStock() {} // TODO: get all children from stock first page
+
+    getDataFromFirstStock() {
+        const firstStock = this.StockChildren[0]
+        const stockId = firstStock.dataset.id // get stock d from dataset
+        const stockTitle = firstStock.querySelector(this.selectors.stockNameSelector).textContent // get stock name from html
+        const stockUrl = `https://kwork.ru/projects/${stockId}/view` // create stock URL
+        const stockPreferredPrice = firstStock.querySelector()
+
+        this.constructObject(
+
+        )
+    }
 }
 
 module.exports = Parser;
